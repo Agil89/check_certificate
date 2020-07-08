718944062567621ad6fcfe18505bfa58eca1a2c9
Ssamirr <sardarlisamir@gmail.com>
2020-07-08 13:21:38 +0400

    first

--- main/views.py
+++ main/views.py
@@ -2,3 +2,10 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'index.html')
+
+def info(request):
+    certificates = Certificate.objects.all()
+    context = {
+        'certificates' : certificates
+    }
+    return render(request,'info.html',context)

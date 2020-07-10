ddad7b353533c725f489634341c2d64115d59ad2
Agil89 <agil.makhmudov@mail.ru>
2020-07-10 12:29:25 +0400

    updated model

--- main/models.py
+++ main/models.py
@@ -7,14 +7,14 @@ class Certificate(models.Model):
 
     """This field is for category types like: food,desert and others"""
     title = models.CharField('Title',max_length=60)
-    certificade_number =models.IntegerField()
+    certificade_number =models.CharField('Certificate number',max_length=50)
     fullname = models.CharField('Fullname',max_length=50)
     image = models.FileField('Image',upload_to = 'certificate_images')
     description = models.TextField('Description')
     instructor = models.CharField('Instructor',max_length=50)
     training_session=models.CharField('Session',max_length=120)
-    started_at = models.DateTimeField(auto_now=True)
-    finished_at = models.DateTimeField(auto_now=True)
+    started_at = models.DateField(auto_now=True)
+    finished_at = models.DateField(auto_now=True)
 
     #moderations
     is_published = models.BooleanField('is published', default=True)
